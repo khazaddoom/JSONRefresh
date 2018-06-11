fbd60061a6900ca0ed392eeff01eed15bb89d3a4
GaneshSAcharya <31121986g@gmail.com>
2018-06-11 10:24:34 +0530

    First Commit to initial Git Repo for Local Project

---
+++ main.js
@@ -0,0 +1,14 @@
+ 
+var jsonObject = {
+    "Name" : "Ganesh S Acharya",
+    "Age" : 31,
+};
+
+function logging() {
+   
+
+    console.log("hello " + jsonObject.Name + " this is my first line of Java Script");
+
+}
+
+setTimeout(logging, 1000);
